6732df2c67b6d8639ede35df69b917f515b7d58c
FrosinaS <frosinastamatoska@gmail.com>
2015-03-05 21:57:31 +0100

    Added description and sanitize

--- src/main/webapp/scripts/components/views/questions/question-details.controller.js
+++ src/main/webapp/scripts/components/views/questions/question-details.controller.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('studentshelpcenterApp')
+    .filter('to_trusted', ['$sce', function($sce){
+        return function(text) {
+            return $sce.trustAsHtml(text);
+        };
+    }])
     .controller('QuestionDetailsController', function ($scope, $stateParams, Question, Answer, Account) {
         $scope.question = {};
         $scope.vote={};
