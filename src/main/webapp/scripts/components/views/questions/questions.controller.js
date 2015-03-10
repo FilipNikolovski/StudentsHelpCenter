edb4064357f3d50eed41856d507f6a783fc11ec2
FrosinaS <frosinastamatoska@gmail.com>
2015-03-11 00:27:21 +0100

    Brishenje na prashanja od questions i od question detail ovozmozeno

--- src/main/webapp/scripts/components/views/questions/questions.controller.js
+++ src/main/webapp/scripts/components/views/questions/questions.controller.js
@@ -7,6 +7,7 @@ angular.module('studentshelpcenterApp')
         });
 
         $scope.questions = [];
+        $scope.deleteQuestion={};
         $scope.loadAll = function() {
             Question.query(function(result) {
                 $scope.questions = result;
@@ -14,7 +15,23 @@ angular.module('studentshelpcenterApp')
         };
         $scope.loadAll();
 
+        $scope.questionDelete=function(question)
+        {
+            $scope.deleteQuestion = question;
+            $('#deleteQuestionConfirmation').modal('show');
+        };
+
+        $scope.confirmQuestionDelete = function () {
+            Question.delete({id: $scope.deleteQuestion.id},
+                function () {
+                    $('#deleteQuestionConfirmation').modal('hide');
+                    $scope.clear();
+                    $scope.loadAll();
+                });
+        };
+
         $scope.clear = function () {
             $scope.question = {title: null, description: null, datePosted: null, solved: null, id: null};
+            $scope.deleteQuestion=null;
         };
     });
