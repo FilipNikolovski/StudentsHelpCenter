e7cbe9440a949172273dadad4f9d4298df364494
FilipNikolovski <filip.nikol92@gmail.com>
2015-03-19 23:45:04 +0100

    fixed question image paths

--- src/main/webapp/scripts/components/views/newQuestion/new-question.controller.js
+++ src/main/webapp/scripts/components/views/newQuestion/new-question.controller.js
@@ -10,9 +10,12 @@ angular.module('studentshelpcenterApp')
         var myDropzone = new Dropzone("div#images-dropzone", {
             url: "/api/questions/upload-images",
             autoProcessQueue: false,
+            addRemoveLinks: true,
+            maxFiles: 5,
+            maxFilesize: 5,
             init: function() {
-                this.on("success", function(file, responseText) {
-                    console.log(responseText);
+                this.on("complete", function(file) {
+                    $location.path('questions/' + $scope.question.id);
                 });
             }
         });
@@ -25,6 +28,7 @@ angular.module('studentshelpcenterApp')
 
             Question.save($scope.question,
                 function (question) {
+                    $scope.question = question;
                     myDropzone.on('sending', function(file, xhr, formData) {
                         formData.append('id', question.id);
                         formData.append('_csrf', $.cookie('CSRF-TOKEN'));
